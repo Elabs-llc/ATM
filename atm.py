bc187b70bd1a3a5e17a2d53016b4a3c25ef5536a
Edwin Asare <edwinasare08@gmail.com>
2024-08-05 13:53:04 -0700

    entry point added to atm.py

--- atm.py
+++ atm.py
@@ -1,15 +1,27 @@
+# This file serve as main init file to load core module neccessary to run this program
+# helper module is imported to load the Helper class which contains
+# the logic and functionalities of this program.
+# 
+# To run this program, run directly from this file `atm.py`
+
 # import helper class
 import helper
-import time
 
-# intialize Helper class
-_helper = helper.Helper()
+# Entry point of ATM Simulation Program
+def init():
+    # intialize Helper class
+    _helper = helper.Helper()
 
-# check if user is logged in
-if(_helper.isLogin):
+    # check if user is logged in
+    if(_helper.isLogin):
 
-    # launch welcome screen
-    _helper.welcome()
+        # launch welcome screen
+        _helper.welcome()
+        
+    else:
+        print(_helper.login()) 
+        
+        
+if __name__ == '__main__':
     
-else:
-    print(_helper.login())
+   init()
